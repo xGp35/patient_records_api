b035ff8f5829f73ae59572122223ae24894d420d
xGp35 <girijaprasanna1999@gmail.com>
2026-05-06 03:05:02 +0530

    new main file

--- main.py
+++ main.py
@@ -1,55 +1,11 @@
 from fastapi import FastAPI, Path, HTTPException, Query
-import json
+from pydantic import BaseModel
 
+import json
 app = FastAPI()
 
 def load_data():
-    with open ('patients.json', 'r') as f:
+    with open('patients.json', 'r') as f:
         data = json.load(f)
-    return data
-
-@app.get("/")
-def hello():
-    return {"message": "Patiner Management System"}
-
-@app.get('/about')
-def about():
-    return {"message": "A fully functional API to manage your patient records"}
-
-@app.get('/view')
-def view():
-    data = load_data()
-
-    return data
-
-
-@app.get('/patient/{patient_id}')
-def view_patient(patient_id: str = Path(
-    ..., description = "ID of the patient in the DB", example = "P001" 
-    )):
-    # load all the patients
-    data = load_data()
-
-    if patient_id in data:
-        return data[patient_id]
-    raise HTTPException(status_code = 404, detail = "Patient not found")
-
-
-@app.get('/sort')
-def sort_patients(
-    sort_by: str = Query(
-        ..., description = 'Sort on the basis of height, weight or bmi'),
-    order: str = Query (
-        ..., description = 'Sort in ascending or descending order'
-    )
-):
-    valid_fields = ['height', 'weight', 'bmi']
-    if sort_by not in valid_fields:
-        raise HTTPException(status_code = 400, detail = f"Invalid field, select from {valid_fields}")
-    if order not in ['asc', 'desc']:
-        raise HTTPException(status_code = 400, detail = f"Invalid field, select from 'asc', 'desc'")
     
-    data = load_data()
-    sort_order = True if order == 'desc' else False
-    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
-    return sorted_data
+    return data
